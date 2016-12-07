79931e901153b2d860120ae4efc8c1d564ec819b
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-12-08 00:06:07 +0100

    Two - Task 2

--- Two/script.py
+++ Two/script.py
@@ -1,21 +1,36 @@
 class pad:
 	def __init__(self):
-		self.keypad = [1,2,3,4,5,6,7,8,9]
-		self.xpos = 1 # horizontal from left
-		self.ypos = 1 # vertical from top
+		self.keypad =  [0,0,1,0,0,
+						0,2,3,4,0,
+						5,6,7,8,9,
+						0,"A","B","C",0,
+						0,0,"D",0,0]
+		self.xpos = 0 # horizontal from left
+		self.ypos = 2 # vertical from top
 
 	def getNumber(self):
-		return self.keypad[self.xpos + 3*self.ypos]
+		return self.keypad[self.xpos + 5*self.ypos]
+
+	def isKeypad(self):
+		return self.getNumber() != 0
 
 	def go(self,dir):
 		if dir == "U" and self.ypos > 0:
 			self.ypos -= 1
-		elif dir == "D" and self.ypos < 2:
+			if(not self.isKeypad()):
+				self.ypos += 1
+		elif dir == "D" and self.ypos < 4:
 			self.ypos += 1
+			if(not self.isKeypad()):
+				self.ypos -= 1
 		elif dir == "L" and self.xpos > 0:
 			self.xpos -= 1
-		elif dir == "R" and self.xpos < 2:
+			if(not self.isKeypad()):
+				self.xpos += 1
+		elif dir == "R" and self.xpos < 4:
 			self.xpos += 1
+			if(not self.isKeypad()):
+				self.xpos -= 1
 
 p = pad()
 output = ""
