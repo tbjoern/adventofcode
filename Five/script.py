0dd2ce3b0fb1ac77c09161b6853c88e5aa1a1fe6
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-12-08 03:25:47 +0100

    Five - task 2

--- Five/script.py
+++ Five/script.py
@@ -1,17 +1,52 @@
 import hashlib
+import os
+import random
+import sys
 
 input = "ugkcyxxp"
-output = ""
+output = []
+for i in range(0,8):
+	output.append("")
+
+hackedChars = 0
 
 salt = 0
+random.seed()
+print
 
-while len(output) < 8:
+def clear():
+	#os.system('cls' if os.name == 'nt' else 'clear')
+	sys.stdout.write("\033[F") #back to previous line
+	sys.stdout.write("\033[K") #clear line
+
+def hacking(pwd):
+	output = "0x"
+	r = random.randint(40,120)
+	clear()
+	for c in pwd:
+		if c == "":
+			output += chr(((r - 30)% 80) + 40)
+			r += 1
+		else:
+			output += c
+	print output
+
+
+while hackedChars < 8:
 	hash = hashlib.md5(input + str(salt)).hexdigest()
 
 	if hash[:5] == "00000":
-		output += hash[5]
-		print "got " + hash[5]
+		try:
+			index = int(hash[5])
+			if 0 <= index and index < 8:
+				if output[index] == "":
+					output[index] = hash[6]
+					hacking(output)
+					hackedChars += 1
+		except Exception as e:
+			a = 1
 
 	salt +=1
 
-print output
+	if salt%1000 == 0:
+		hacking(output)
